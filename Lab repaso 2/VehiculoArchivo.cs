f68d15fbf3856a9664658ac8c9783f398f70de74
vickychoto <victoriasotocastil@gmail.com>
2025-04-07 08:48:20 -0600

    Completo

--- Lab repaso 2/VehiculoArchivo.cs
+++ Lab repaso 2/VehiculoArchivo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,18 +10,22 @@ namespace Lab_repaso_2
 {
     internal class VehiculoArchivo
     {
-        string json = JsonConvert.SerializeObject(vehiculos);
-        System.IO.File.WriteAllText(archivo, json);
-
+        public void Guardar(string archivo, List<Vehiculo> vehiculos)
+        {
+            string json = JsonConvert.SerializeObject(vehiculos);
+            System.IO.File.WriteAllText(archivo, json);
         }
-    public List<Vehiculo> Leer(string archivo)
-    {
-        List<Vehiculo> lista = new List<Vehiculo>();
-        StreamReader jsonStream = File.OpenText(archivo);
-        string json = jsonStream.ReadToEnd();
-        jsonStream.Close();
 
-        lista = JsonConvert.DeserializeObject<List<Vehiculo>>(json);
-        return lista;
+        public List<Vehiculo> Leer(string archivo)
+        {
+            List<Vehiculo> lista = new List<Vehiculo>();
+            StreamReader jsonStream = File.OpenText(archivo);
+            string json = jsonStream.ReadToEnd();
+            jsonStream.Close();
+
+            lista = JsonConvert.DeserializeObject<List<Vehiculo>>(json);
+            return lista;
+        }
     }
+    
 }
